833406393d4276c764706cb403663abfcc5c6073
Auro <35087590+anqueue@users.noreply.github.com>
2025-10-28 14:58:31 -0500

    dev: migrate to SPA

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,8 @@
 import { Button } from "@mantine/core";
-import { Link, MetaFunction, redirect } from "@remix-run/react";
+import { Link, MetaFunction } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [{ title: "Homepage" }, { name: "description", content: "Homepage" }];
-};
-
-export const loader = async () => {
-  return redirect("/calculator");
 };
 
 export default function Index() {
